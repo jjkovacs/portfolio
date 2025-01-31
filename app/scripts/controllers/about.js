4980b7576caabf981e46928f83053ba266d0d16a
James Kovacs <jjkovacs89@gmail.com>
2025-01-31 17:40:28 -0500

    Added more images and tweaked some text.

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -6,10 +6,13 @@ angular.module('personalApp')
       header: "A Little About Me",
       paragraphs: [
         "Hello! I'm James. Nice to meet you!",
-        "My passion for software development started when I was in high school. As a hobby, I would build custom web layouts/templates for social media websites, and I had a Freewebs site where I hosted all of the different layouts I had created. In 2007, I was accepted into University of Michigan's College of Engineering, and I decided to turn this hobby into a career.",
+        "My passion for software development started when I was in high school. As a hobby, I would build custom web layouts/templates for social media websites. I had a Freewebs site where I hosted all of the different layouts I had created. In 2007, I was accepted into University of Michigan's College of Engineering, and I decided to turn this hobby into a career.",
         "I graduated in 2011 and began my career at New World Systems, working on public safety software. I had the opportunity to work on many different projects, and many different technologies, which was an incredible learning experience for a new developer.",
         "After a few years, I was ready to take my career in a different direction. Having grown up in the Metro Detroit area, I was interested in working in the automotive industry. In 2015 I was hired by GM to work on the infotainment system, and have been there ever since.",
-        "Outside of work, I enjoy spending time with my family. I'm an avid sports fan, and I enjoy watching and attending games for all of the local sports teams. On fall Saturdays, you can find me out on the golf course in Ann Arbor tailgating for Michigan football (Go Blue!).",
+
+        "<img src=\"../../assets/images/fam.jpg\" class=\"info-img\" />",
+
+        "Outside of work, I enjoy spending time with my family. I'm an avid sports fan. I enjoy watching and attending games for all of the local sports teams, and cheering on my daughter in her swim competitions. On fall Saturdays, you can find me out on the golf course in Ann Arbor tailgating for Michigan football (Go Blue!).",
         "Thank you for stopping by! If you would like to learn more about my experience, please check out the other sections on this site!"
       ]
     };
--- app/scripts/controllers/education.js
+++ app/scripts/controllers/education.js
@@ -5,9 +5,15 @@ angular.module('personalApp')
     $scope.content = {
       header: "University of Michigan",
       paragraphs: [
-        "I attended the University of Michigan from 2007-2011. I was a student in the College of Engineering, and earned a BSE in Computer Science Engineering.",
-        "In 2010, during the summer between my junior and senior year, I interned at Parametric Technology Corporation. I joined a team that supported <a href=\"https://www.ptc.com/en/products/arbortext\">Arbortext Editor</a>, a text editing application that businesses use for authoring technical documentation and manuals.",
-        "For my senior design class, I took a video game development course. I paired up with two other classmates, and we built a video game we called RAPTORS. It was a top-down shooter where the players attempt to get through levels while being attacked by dinosaurs. The game used Wii Remotes for input, and the user could aim anywhere on screen to shoot at enemies. It was very well-received at the expo, and we ended up winning 3rd place in the competition for best game. A trailer for the game can be seen <a href=\"https://www.youtube.com/watch?v=_CuuL-KYpdQ\">here</a>."
+        "I attended the University of Michigan from 2007-2011. I was a student in the College of Engineering. I graduated Cum Laude in 2011 with a degree in Computer Science Engineering.",
+
+        "<img src=\"../../assets/images/michigan_union.jpg\" class=\"info-img\" />",
+
+        "In 2010, during the summer between my junior and senior year, I interned at Parametric Technology Corporation. I joined a team that supported <a href=\"https://www.ptc.com/en/products/arbortext\">Arbortext Editor</a>, a text editing application that businesses use for authoring technical documentation and manuals. During my internship, I worked on app localization, and a feature to improve the table editing tool within the application.",
+
+        "<img src=\"../../assets/images/cs_belltower.jpg\" class=\"info-img\" />",
+
+        "For my senior design class, I took a video game development course. I paired up with two other classmates, and we built a video game we called <a href=\"https://www.youtube.com/watch?v=_CuuL-KYpdQ\">RAPTORS</a>. It was a top-down shooter where the players attempt to get through levels while being attacked by dinosaurs. The game used Wii Remotes for input, and the user could aim anywhere on screen to shoot at enemies. It was very well-received at the expo, and we ended up winning 3rd place in the competition for best game."
       ]
     };
     
